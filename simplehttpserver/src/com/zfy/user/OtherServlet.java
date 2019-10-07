b499158b682493d7aff58e30a490942ae99309b5
DELL <DELL@DESKTOP-3OU3NNV>
2019-10-07 19:57:29 +0800

    simulate the tomcat

--- simplehttpserver/src/com/zfy/user/OtherServlet.java
+++ simplehttpserver/src/com/zfy/user/OtherServlet.java
@@ -10,6 +10,8 @@ public class OtherServlet implements Servlet{
 	public void service(Request request, Respond respond) {
 		// TODO Auto-generated method stub
 		
+		
+		
 		respond.print("<html><head><title>HTTP</title><head><body>"
 				+"-->this is the servlet of other!!<--"
 				+request.getParameter("uname")
